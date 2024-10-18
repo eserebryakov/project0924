61537e3b0cec897977092945684b95a6b82b57c5
Евгений Серебряков <ENiSerebryakov@rosatom.ru>
2024-10-18 14:46:51 +0300

    Написал тест test_there_is_one_root и написал минимальную реализацию фунцкии solve под этот тест. Добавил epsilon. ДЗ пункты 7,8

--- src/quadratic_equation.py
+++ src/quadratic_equation.py
@@ -3,11 +3,14 @@ import cmath
 
 class QuadraticEquation:
     @staticmethod
-    def solve(a, b, c):
+    def solve(a, b, c, e=1e-5):
         discriminant = b * b - 4 * a * c
 
-        if discriminant < 0:
+        if discriminant < -e:
             return []
 
-        if discriminant > 0:
+        if abs(discriminant) <= e:
+            return -b / (2 * a), -b / (2 * a)
+
+        if discriminant > e:
             return -b + cmath.sqrt(discriminant) / 2.0 / a, -b - cmath.sqrt(discriminant) / 2.0 / a
--- tests/test_quadratic_equation.py
+++ tests/test_quadratic_equation.py
@@ -15,3 +15,7 @@ class TestQuadraticEquation:
     def test_there_are_two_roots(self):
         """П5. Тест проверяет, что для уравнения x^2-1 = 0 есть два корня."""
         assert self.solve(a=1, b=0, c=-1) == (1.0, -1.0)
+
+    def test_there_is_one_root(self):
+        """П7. Тест проверяет, что для уравнения x^2+2x+1 = 0 есть один корень."""
+        assert self.solve(a=1, b=2, c=1) == (-1.0, -1.0)
