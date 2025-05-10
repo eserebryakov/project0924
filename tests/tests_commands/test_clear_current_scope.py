e7ca56add8552203d6cf1e2c9c6268c6a69fbde3
Серебряков Евгений Николаевич <ENiSerebryakov@rosatom.ru>
2025-05-10 12:58:23 +0300

    jhgkjhg

--- tests/tests_commands/test_clear_current_scope.py
+++ tests/tests_commands/test_clear_current_scope.py
@@ -7,12 +7,18 @@ from src.spacebattle.commands.init import InitCommand
 class TestClearCurrentScopeCommand:
     """Тест проверяющий команду, очищающую текущий scope."""
 
-    @pytest.fixture(autouse=True)
-    def setup(self):
+    @pytest.fixture(scope="function")
+    def original_scope(self, request):
+        current_scope = InitCommand.current_scope
         InitCommand.current_scope.value = 1
         self.clear_command = ClearCurrentScopeCommand()
 
-    def test_clear_current_scope(self):
+        def teardown():
+            InitCommand.current_scope = current_scope
+
+        request.addfinalizer(teardown)
+
+    def test_clear_current_scope(self, original_scope):
         """П4. Тест проверяет команду, которая очищает текущий scope."""
         self.clear_command.execute()
         with pytest.raises(AttributeError):
