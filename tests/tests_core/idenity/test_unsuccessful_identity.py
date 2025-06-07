91367c3987c77df993de303c5c7a068d58b713a2
Серебряков Евгений Николаевич <ENiSerebryakov@rosatom.ru>
2025-06-07 17:46:09 +0300

    kjhgjkhg

--- tests/tests_core/idenity/test_unsuccessful_identity.py
+++ tests/tests_core/idenity/test_unsuccessful_identity.py
@@ -18,7 +18,7 @@ TEST_OBJECT = {
 }
 
 
-class TestSuccessfulIdentity:
+class TestUnsuccessfulIdentity:
     @pytest.fixture(scope="function")
     def initial_state(self, request):
         InitCommand.root_scope = {}
@@ -34,27 +34,24 @@ class TestSuccessfulIdentity:
             url="http://127.0.0.1:5000/api/games/start_sec",
             json={"participants": [self.user_id_1, self.user_id_2, self.user_id_3]},
         )
-        self.game_id = response.json()["game_id"]
+        self.game_id_1 = response.json()["game_id"]
         response = self.session.post(
-            url="http://127.0.0.1:5001/api/auth/token", json={"game_id": self.game_id, "user_id": self.user_id_1}
+            url="http://127.0.0.1:5001/api/auth/token", json={"game_id": self.game_id_1, "user_id": self.user_id_1}
         )
-        print(response.text)
-        self.token = response.json()["access_token"]
-        self.object_id = TEST_OBJECT["object_id"]
+        self.token_1 = response.json()["access_token"]
 
-        message = Message(
-            game_id=self.game_id,
-            object_id=self.object_id,
-            operation_id=constants.OPERATION_CREATE_OBJECT,
-            args=TEST_OBJECT,
-        )
+        self.user_id_4 = str(uuid4())
         response = self.session.post(
-            url="http://127.0.0.1:5000/api/games/messages_sec",
-            headers={
-                "Authorization": "Bearer " + self.token,
-            },
-            json=message.model_dump_json(),
+            url="http://127.0.0.1:5000/api/games/start_sec",
+            json={"participants": [self.user_id_4]},
         )
+        self.game_id_2 = response.json()["game_id"]
+        response = self.session.post(
+            url="http://127.0.0.1:5001/api/auth/token", json={"game_id": self.game_id_2, "user_id": self.user_id_4}
+        )
+        self.token_2 = response.json()["access_token"]
+
+        self.object_id = TEST_OBJECT["object_id"]
 
         def teardown():
             InitCommand.root_scope = {}
@@ -66,18 +63,18 @@ class TestSuccessfulIdentity:
 
         request.addfinalizer(teardown)
 
-    def test_successful_identity_moving_object(self, initial_state):
+    def test_unsuccessful_identity_moving_object(self, initial_state):
         message = Message(
-            game_id=self.game_id,
+            game_id=self.game_id_1,
             object_id=self.object_id,
-            operation_id=constants.OPERATION_MOVING_STRAIGHT_LINE,
-            args=Vector(100, 100),
+            operation_id=constants.OPERATION_CREATE_OBJECT,
+            args=TEST_OBJECT,
         )
         response = self.session.post(
             url="http://127.0.0.1:5000/api/games/messages_sec",
             headers={
-                "Authorization": "Bearer " + self.token,
+                "Authorization": "Bearer " + self.token_2,
             },
             json=message.model_dump_json(),
         )
-        assert response.json()["args"]["location"] == Vector(102, 102).model_dump()
+        assert response.status_code == 403
