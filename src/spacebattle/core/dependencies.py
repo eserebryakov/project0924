02076c627793e23bf6a6c47579b73ab191880ad1
Серебряков Евгений Николаевич <ENiSerebryakov@rosatom.ru>
2025-06-11 12:24:31 +0300

    Прочие правки di

--- src/spacebattle/core/dependencies.py
+++ src/spacebattle/core/dependencies.py
@@ -6,13 +6,11 @@ from src.spacebattle.scopes.ioc import IoC
 
 def spaceship_move(*args, queue: Queue):
     """Команда для постоянного движения объекта"""
-    commands_list = IoC.resolve(constants.RULE_SPACESHIP_MOVE)
-    commands = []
-    for command in commands_list:
-        commands.append(IoC.resolve(command, *args))
-    macro = IoC.resolve(constants.COMMAND_MACRO, commands)
-    result = IoC.resolve(constants.COMMAND_INJECTABLE)
-    repeater = IoC.resolve(constants.COMMAND_PUT_COMMAND_TO_QUEUE, result, queue)
-    internal_macro_command = IoC.resolve(constants.COMMAND_MACRO, [macro, repeater])
-    result.inject(internal_macro_command)
-    return result
+    macro_move_command = IoC.resolve(
+        constants.COMMAND_MACRO, [IoC.resolve(command, *args) for command in IoC.resolve(constants.RULE_SPACESHIP_MOVE)]
+    )
+    injectable_command = IoC.resolve(constants.COMMAND_INJECTABLE)
+    put_command = IoC.resolve(constants.COMMAND_PUT_COMMAND_TO_QUEUE, injectable_command, queue)
+    internal_macro_command = IoC.resolve(constants.COMMAND_MACRO, [macro_move_command, put_command])
+    injectable_command.inject(internal_macro_command)
+    return injectable_command
